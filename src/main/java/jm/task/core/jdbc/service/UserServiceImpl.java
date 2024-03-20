27b9559c8e09b1b7f4c888679b7bc71e15c5b7d7
Alexey Gorbunov <alexgorbunov95@mail.ru>
2024-03-20 17:50:31 +0300

    Rabota nad oshibkami 3

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,12 +1,13 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
+    private UserDao userDaoHibernate = new UserDaoHibernateImpl();
 
     @Override
     public void createUsersTable() {
@@ -22,7 +23,6 @@ public class UserServiceImpl implements UserService {
     public void saveUser(String name, String lastName, byte age) {
         userDaoHibernate.saveUser(name, lastName, age);
         System.out.println("User с именем " + name + " добавлен в базу данных");
-
     }
 
     @Override
